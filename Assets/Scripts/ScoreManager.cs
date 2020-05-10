cf851c529cc97be4fedfc2b9000e16e14c94ee3d
fadilaahmads <fadilahmad59@gmail.com>
2020-05-10 20:32:26 +0700

    Added Tutorial Pop Up

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -10,7 +10,7 @@ public class ScoreManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        scoreDisplay.text = score.ToString();   
+    
     }
 
     // Update is called once per frame
